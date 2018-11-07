82a6448692b365aa153549187b55ce5465283023
Joshua Welsh <Jawshua@users.noreply.github.com>
2018-11-07 10:46:21 +0000

    Ensure backslashes are always escaped

--- sjson_test.go
+++ sjson_test.go
@@ -118,7 +118,7 @@ func TestBasic(t *testing.T) {
 		``,
 		"\\:1.this.4", `4`)
 	testRaw(t, setRaw,
-		`{":\1":{"this":[null,null,null,null,{".HI":4}]}}`,
+		`{":\\1":{"this":[null,null,null,null,{".HI":4}]}}`,
 		``,
 		"\\:\\\\1.this.4.\\.HI", `4`)
 	testRaw(t, setRaw,
